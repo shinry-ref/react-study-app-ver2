6ca041b6beba67270156473546f7f162a5b12ebd
refdoragon <refdoragon>
2024-09-03 00:14:41 +0900

    ローディング画面が表示されること

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -2,22 +2,15 @@ import App from "../App";
 import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
 
-// 例: Jestを使ったモック
-jest.mock('../utils/supabaseFunction', () => ({
-  getAllStudyRecords: jest.fn(),
-  deleteStudyRecord: jest.fn(),
-}));
-
-
-describe("title", () => {
+describe("App Component", () => {
   it("ローディング画面が表示されること", async () => {
-    render(<App />);
+    // `loading`状態を`true`に設定してAppコンポーネントをレンダリング
+    render(<App/>);
+    
+    // `Spinner`が表示されるのを待つ
     const spinner = await screen.findByTestId("spinner");
     expect(spinner).toBeInTheDocument();
   });
 
-  // it("should render title", () => {
-  //   render(<App />);
-  //   expect(screen.getByText("Hello World")).toBeInTheDocument();
-  // });
+  // 他のテストケースもここに追加できます
 });
